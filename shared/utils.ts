7e3740f36f0c955637f0e5b10d5a36ee37f68df6
EmanuelRaileanu <raileanuemanuelalin@gmail.com>
2022-01-27 22:32:33 +0200

    Implement custom class-validator decorator
    - Implemented 2 decorators for matching and not matching fields in request bodies

--- shared/utils.ts
+++ shared/utils.ts
@@ -3,6 +3,14 @@ import * as morgan from "morgan";
 import { INestApplication, ValidationPipe } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { SwaggerConfig } from "./entities";
+import {
+    registerDecorator,
+    ValidationArguments,
+    ValidationOptions,
+    ValidatorConstraint,
+    ValidatorConstraintInterface
+} from "class-validator";
+import { ClassConstructor } from "class-transformer";
 
 const setupSwagger = (app: INestApplication, swaggerConfig: SwaggerConfig) => {
     if (!swaggerConfig.swaggerDoc) {
@@ -31,3 +39,61 @@ export const bootstrapServer = async (module: any, port: number, swaggerConfig?:
     await app.listen(port);
     console.log("Server listening on port " + port);
 };
+
+export const Match = <T>(
+    type: ClassConstructor<T>,
+    property: (o: T) => any,
+    validationOptions?: ValidationOptions,
+) => {
+    return (object: any, propertyName: string) => {
+        registerDecorator({
+            target: object.constructor,
+            propertyName,
+            options: validationOptions,
+            constraints: [property],
+            validator: MatchConstraint
+        });
+    };
+};
+
+@ValidatorConstraint({ name: "Match" })
+export class MatchConstraint implements ValidatorConstraintInterface {
+    public validate(value: any, args: ValidationArguments) {
+        const [fn] = args.constraints;
+        return fn(args.object) === value;
+    }
+
+    public defaultMessage(args: ValidationArguments) {
+        const [constraintProperty]: (() => any)[] = args.constraints;
+        return `${constraintProperty} should be equal to ${args.property}`;
+    }
+}
+
+export const NotMatch = <T>(
+    type: ClassConstructor<T>,
+    property: (o: T) => any,
+    validationOptions?: ValidationOptions,
+) => {
+    return (object: any, propertyName: string) => {
+        registerDecorator({
+            target: object.constructor,
+            propertyName,
+            options: validationOptions,
+            constraints: [property],
+            validator: NotMatchConstraint
+        });
+    };
+};
+
+@ValidatorConstraint({ name: "NotMatch" })
+export class NotMatchConstraint implements ValidatorConstraintInterface {
+    public validate(value: any, args: ValidationArguments) {
+        const [fn] = args.constraints;
+        return fn(args.object) !== value;
+    }
+
+    public defaultMessage(args: ValidationArguments) {
+        const [constraintProperty]: (() => any)[] = args.constraints;
+        return `${constraintProperty} should not be equal to ${args.property}`;
+    }
+}
