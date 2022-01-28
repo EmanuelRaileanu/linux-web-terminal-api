cfe30bbd4f43d617504884ddfd03d4c91404e60f
EmanuelRaileanu <raileanuemanuelalin@gmail.com>
2022-01-28 23:30:59 +0200

    Write unit tests for the controllers

---
+++ auth/test/auth.controller.spec.ts
@@ -0,0 +1,160 @@
+import { AuthService } from "../src/services/auth.service";
+import { User } from "../src/entities/db/user.entity";
+import { getRepository, Repository } from "typeorm";
+import { JwtModule, JwtService } from "@nestjs/jwt";
+import { Test, TestingModule } from "@nestjs/testing";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { config } from "../src/config";
+import { PassportModule } from "@nestjs/passport";
+import { CacheModule } from "@nestjs/common";
+import { UserService } from "../src/services/user.service";
+import { LocalStrategy } from "../src/strategies/local.strategy";
+import { JwtStrategy } from "../src/strategies/jwt.strategy";
+import * as bcrypt from "bcryptjs";
+import { AuthController } from "../src/controllers/auth.controller";
+import { RegisterRequest, UserEntityHolder } from "../src/entities/auth.entities";
+import { JwtResponse } from "../src/entities/jwt.entities";
+import { PasswordsDoNotMatchError, UserAlreadyExistsError } from "../src/errors";
+
+describe(AuthController, () => {
+    let users: User[];
+    let userRepository: Repository<User>;
+    let authController: AuthController;
+    let jwtService: JwtService;
+    let moduleRef: TestingModule;
+
+    beforeAll(async () => {
+        moduleRef = await Test.createTestingModule({
+            imports: [
+                TypeOrmModule.forFeature([User]),
+                TypeOrmModule.forRoot({
+                    type: "mysql",
+                    ...config.db,
+                    database: config.testDb,
+                    autoLoadEntities: true,
+                    synchronize: true
+                }),
+                PassportModule,
+                JwtModule.register({
+                    secret: config.jwt.secret,
+                    signOptions: { expiresIn: config.jwt.lifespan }
+                }),
+                CacheModule.register()
+            ],
+            controllers: [AuthController],
+            providers: [AuthService, UserService, LocalStrategy, JwtStrategy]
+        }).compile();
+
+        authController = moduleRef.get<AuthController>(AuthController);
+        userRepository = getRepository<User>(User);
+        jwtService = moduleRef.get<JwtService>(JwtService);
+
+        users = [
+            {
+                id: "89e5513c-e455-4561-8717-dda74601e6f0",
+                username: "rick",
+                email: "rick@roll.com",
+                password: await bcrypt.hash("password", await bcrypt.genSalt()),
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                id: "b3dcf1c5-4de8-41e9-b5ce-d882858e9940",
+                username: "karen",
+                email: "karen@email.com",
+                password: await bcrypt.hash("joker", await bcrypt.genSalt()),
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+        ];
+        await userRepository.insert(users);
+    });
+
+    afterAll(async () => {
+        await userRepository.remove(users);
+        return moduleRef.close();
+    });
+
+    test("Register successfully", async () => {
+        const registerRequest: RegisterRequest = {
+            username: "Galactus123",
+            email: "classified@world.com",
+            password: "password",
+            confirmedPassword: "password"
+        };
+        await expect(authController.register(registerRequest)).resolves.toEqual(undefined);
+        const createdUser = await userRepository.findOne({ username: registerRequest.username });
+        users.push(createdUser as User);
+        expect(registerRequest.username).toEqual(createdUser?.username);
+        expect(registerRequest.email).toEqual(createdUser?.email);
+        return expect(bcrypt.compare(registerRequest.password, createdUser?.password as string)).resolves.toEqual(true);
+    });
+
+    test("Register fails with UserAlreadyExistsError when providing a username that is already in use", () => {
+        const registerRequest: RegisterRequest = {
+            username: "rick",
+            email: "classified@world.com",
+            password: "password",
+            confirmedPassword: "password"
+        };
+        return expect(authController.register(registerRequest)).rejects.toThrowError(UserAlreadyExistsError);
+    });
+
+    test("Register fails with UserAlreadyExistsError when providing an email that is already in use", () => {
+        const registerRequest: RegisterRequest = {
+            username: "kara",
+            email: "rick@roll.com",
+            password: "password",
+            confirmedPassword: "password"
+        };
+        return expect(authController.register(registerRequest)).rejects.toThrowError(UserAlreadyExistsError);
+    });
+
+    test("Register fails with PasswordsDoNotMatchError when password is different from confirmedPassword", () => {
+        const registerRequest: RegisterRequest = {
+            username: "kara",
+            email: "kara@email.com",
+            password: "password",
+            confirmedPassword: "inception"
+        };
+        return expect(authController.register(registerRequest)).rejects.toThrowError(PasswordsDoNotMatchError);
+    });
+
+    test("Login successfully", () => {
+        const userEntityHolder: UserEntityHolder = {
+            user: {
+                id: users[0].id,
+                email: users[0].email,
+                username: users[0].username
+            }
+        };
+        const expectedResponse: JwtResponse = {
+            token: jwtService.sign(userEntityHolder.user)
+        };
+        return expect(authController.login(userEntityHolder)).resolves.toEqual(expectedResponse);
+    });
+
+    test("Logout successfully", () => {
+        const userEntityHolder: UserEntityHolder = {
+            user: {
+                id: users[0].id,
+                email: users[0].email,
+                username: users[0].username
+            }
+        };
+        return expect(authController.logout(userEntityHolder)).resolves.toEqual(undefined);
+    });
+
+    test("Validate token - returns currently logged in user", () => {
+        const userEntityHolder: UserEntityHolder = {
+            user: {
+                id: users[0].id,
+                email: users[0].email,
+                username: users[0].username,
+                iat: Date.now(),
+                exp: Date.now() + config.jwt.lifespan * 1000
+            }
+        };
+        return expect(authController.validateToken(userEntityHolder)).toEqual(userEntityHolder.user);
+    });
+});
---
+++ auth/test/user.controller.spec.ts
@@ -0,0 +1,177 @@
+import { UserService } from "../src/services/user.service";
+import { User } from "../src/entities/db/user.entity";
+import { getRepository, Repository } from "typeorm";
+import { Test, TestingModule } from "@nestjs/testing";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { config } from "../src/config";
+import { CacheModule } from "@nestjs/common";
+import * as bcrypt from "bcryptjs";
+import { UserController } from "../src/controllers/user.controller";
+import { ChangeEmailRequest, ChangePasswordRequest, ChangeUsernameRequest } from "../src/entities/user.entities";
+import { SessionUserEntity, UserEntityHolder } from "../src/entities/auth.entities";
+import { PasswordsDoNotMatchError, UserNotFoundError, WrongPasswordError } from "../src/errors";
+import { JwtModule } from "@nestjs/jwt";
+
+describe(UserService, () => {
+    let users: User[];
+    let userRepository: Repository<User>;
+    let userController: UserController;
+    let moduleRef: TestingModule;
+    let userEntityHolder: UserEntityHolder;
+
+    beforeAll(async () => {
+        moduleRef = await Test.createTestingModule({
+            imports: [
+                TypeOrmModule.forFeature([User]),
+                TypeOrmModule.forRoot({
+                    type: "mysql",
+                    ...config.db,
+                    database: config.testDb,
+                    autoLoadEntities: true,
+                    synchronize: true
+                }),
+                JwtModule.register({
+                    secret: config.jwt.secret,
+                    signOptions: { expiresIn: config.jwt.lifespan }
+                }),
+                CacheModule.register()
+            ],
+            controllers: [UserController],
+            providers: [UserService]
+        }).compile();
+
+        userController = moduleRef.get<UserController>(UserController);
+        userRepository = getRepository<User>(User);
+        users = [
+            {
+                id: "929a511c-a390-457e-8eb8-aab468ea93f5",
+                username: "carl",
+                email: "carl@email.com",
+                password: await bcrypt.hash("password", await bcrypt.genSalt()),
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                id: "3ceeb3d0-09d5-4cb5-a5a6-c40e9d1b1462",
+                username: "irina",
+                email: "irina@email.com",
+                password: await bcrypt.hash("password", await bcrypt.genSalt()),
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+        ];
+        userEntityHolder = { user: users[0] };
+        await userRepository.insert(users);
+    });
+
+    afterAll(async () => {
+        await userRepository.remove(users);
+        return moduleRef.close();
+    });
+
+    test("Change username", async () => {
+        const changeUsernameRequest: ChangeUsernameRequest = {
+            newUsername: "scott",
+            password: "password"
+        };
+        const promise = userController.changeUsername(userEntityHolder, changeUsernameRequest);
+        await expect(promise).resolves.toEqual(undefined);
+        users[0] = { ...users[0], username: changeUsernameRequest.newUsername };
+        const updatedUser = await userRepository.findOne(users[0].id);
+        return expect(updatedUser?.username).toEqual(users[0].username);
+    });
+
+    test("Changing username fails with UserNotFoundError when the user doesn't exist", () => {
+        const changeUsernameRequest: ChangeUsernameRequest = {
+            newUsername: "scott",
+            password: "password"
+        };
+        const user = { id: "3408ba41-3b24-4e17-b589-e8b9af2d1d8c" } as SessionUserEntity;
+        const promise = userController.changeUsername({ user }, changeUsernameRequest);
+        return expect(promise).rejects.toThrowError(UserNotFoundError);
+    });
+
+    test("Changing username fails with WrongPasswordError when providing the wrong current password", () => {
+        const changeUsernameRequest: ChangeUsernameRequest = {
+            newUsername: "scott",
+            password: "desktop"
+        };
+        const promise = userController.changeUsername(userEntityHolder, changeUsernameRequest);
+        return expect(promise).rejects.toThrowError(WrongPasswordError);
+    });
+
+    test("Change email", async () => {
+        const changeEmailRequest: ChangeEmailRequest = {
+            newEmail: "scott@castle.com",
+            password: "password"
+        };
+        const promise = userController.changeEmail(userEntityHolder, changeEmailRequest);
+        await expect(promise).resolves.toEqual(undefined);
+        users[0] = { ...users[0], email: changeEmailRequest.newEmail };
+        const updatedUser = await userRepository.findOne(users[0].id);
+        return expect(updatedUser?.email).toEqual(users[0].email);
+    });
+
+    test("Changing email fails with UserNotFoundError when the user doesn't exist", () => {
+        const changeEmailRequest: ChangeEmailRequest = {
+            newEmail: "scott@castiglioni.com",
+            password: "password"
+        };
+        const user = { id: "3408ba41-3b24-4e17-b589-e8b9af2d1d8c" } as SessionUserEntity;
+        const promise = userController.changeEmail({ user }, changeEmailRequest);
+        return expect(promise).rejects.toThrowError(UserNotFoundError);
+    });
+
+    test("Changing email fails with WrongPasswordError when providing the wrong current password", () => {
+        const changeEmailRequest: ChangeEmailRequest = {
+            newEmail: "scott@castiglioni@gmail.com",
+            password: "desktop"
+        };
+        const promise = userController.changeEmail(userEntityHolder, changeEmailRequest);
+        return expect(promise).rejects.toThrowError(WrongPasswordError);
+    });
+
+    test("Change password", async () => {
+        const changePasswordRequest: ChangePasswordRequest = {
+            currentPassword: "password",
+            newPassword: "sparta",
+            confirmedNewPassword: "sparta"
+        };
+        const promise = userController.changePassword(userEntityHolder, changePasswordRequest);
+        await expect(promise).resolves.toEqual(undefined);
+        const updatedUser = await userRepository.findOne(users[0].id);
+        users[0] = { ...users[0], password: updatedUser?.password as string };
+        return expect(await bcrypt.compare(changePasswordRequest.newPassword, updatedUser?.password as string));
+    });
+
+    test("Change password fails with UserNotFoundError when the user doesn't exist", () => {
+        const changePasswordRequest: ChangePasswordRequest = {
+            currentPassword: "password",
+            newPassword: "sparta",
+            confirmedNewPassword: "sparta"
+        };
+        const user = { id: "3408ba41-3b24-4e17-b589-e8b9af2d1d8c" } as SessionUserEntity;
+        const promise = userController.changePassword({ user }, changePasswordRequest);
+        return expect(promise).rejects.toThrowError(UserNotFoundError);
+    });
+
+    test("Change password fails with WrongPasswordError when providing the wrong current password", () => {
+        const changePasswordRequest: ChangePasswordRequest = {
+            currentPassword: "fishy",
+            newPassword: "sparta",
+            confirmedNewPassword: "sparta"
+        };
+        const promise = userController.changePassword(userEntityHolder, changePasswordRequest);
+        return expect(promise).rejects.toThrowError(WrongPasswordError);
+    });
+
+    test("Change password fails with WrongPasswordError when passwords do not match", () => {
+        const changePasswordRequest: ChangePasswordRequest = {
+            currentPassword: "sparta",
+            newPassword: "spear",
+            confirmedNewPassword: "spearhead"
+        };
+        const promise = userController.changePassword(userEntityHolder, changePasswordRequest);
+        return expect(promise).rejects.toThrowError(PasswordsDoNotMatchError);
+    });
+});
