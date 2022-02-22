b2eae73f6bde54c8121aa31e83898c54e7d583e1
EmanuelRaileanu <raileanuemanuelalin@gmail.com>
2022-02-22 21:16:15 +0200

    Fix vm-manager startup

--- vm-manager/src/modules/vm-instance.module.ts
+++ vm-manager/src/modules/vm-instance.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { VmInstance } from "@shared/db-entities/vm-instance.entity";
 import { VmInstanceService } from "../services/vm-instance.service";
+import { User } from "@shared/db-entities/user.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([VmInstance])],
+    imports: [TypeOrmModule.forFeature([VmInstance, User])],
     providers: [VmInstanceService],
     exports: [TypeOrmModule, VmInstanceService]
 
