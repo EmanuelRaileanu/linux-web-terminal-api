f98b410c448ee96cc42407b8d6bca126e8838a02
EmanuelRaileanu <raileanuemanuelalin@gmail.com>
2022-05-01 16:59:10 +0300

    Add debug logs

--- vm-manager/src/services/virt-install.service.ts
+++ vm-manager/src/services/virt-install.service.ts
@@ -82,7 +82,7 @@ export class VirtInstallService implements IVirtInstallService {
         if (stderr) {
             throw new VirtInstallError(stderr);
         }
-        console.log(user);
+        console.log(user, user.id);
         const vmInstanceEntity = await this.vmInstanceService.create(user, {
             name: vmName,
             username: options.username,
