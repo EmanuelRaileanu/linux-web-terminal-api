bc69cfcf48038d944582ce997705baf5e7d24a7a
EmanuelRaileanu <raileanuemanuelalin@gmail.com>
2022-06-20 22:51:02 +0300

    Fix SIGINT signal

--- ssh-service/src/services/ssh.service.ts
+++ ssh-service/src/services/ssh.service.ts
@@ -36,7 +36,7 @@ export class SSHService {
 
     private handleSignals(ws: Socket, shellStream: ClientChannel) {
         ws.on("SIGINT", () => {
-            shellStream.signal("SIGINT");
+            shellStream.signal("INT");
         });
     }
 }
