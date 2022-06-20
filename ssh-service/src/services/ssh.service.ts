9754475b7e49ec9462a202aed6577323e112d915
EmanuelRaileanu <raileanuemanuelalin@gmail.com>
2022-06-20 22:53:00 +0300

    Add logging for SIGINT

--- ssh-service/src/services/ssh.service.ts
+++ ssh-service/src/services/ssh.service.ts
@@ -36,6 +36,7 @@ export class SSHService {
 
     private handleSignals(ws: Socket, shellStream: ClientChannel) {
         ws.on("SIGINT", () => {
+            console.log("SIGINT received");
             shellStream.signal("INT");
         });
     }
